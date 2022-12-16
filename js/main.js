348c2b73f422e2a6a605a4f286d1a332d96ea6ee
ngames365 <116061334+ngames365@users.noreply.github.com>
2022-12-16 17:59:15 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var gaenabled = window.localStorage.getItem("ga");
+if (gaenabled == "false") {
+  script("Skipped GA injection because it is disabled by the user.");
+} else {
+  const gascript = document.createElement("script");
+  gascript.setAttribute("async", "");
+  gascript.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-98DP5VKS42");
+  const inlinegascript = document.createElement("script");
+  inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-TREXYT1HKX');`;
+  document.head.append(gascript, inlinegascript);
+  script("Injected script 1/5");
+}
