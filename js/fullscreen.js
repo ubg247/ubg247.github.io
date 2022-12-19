b4aed6662000870bb9ecdf49052ac09129589082
khanhcx5 <khanhcx5@gmail.com>
2022-12-19 09:28:03 +0700

    update

---
+++ js/fullscreen.js
@@ -0,0 +1,37 @@
+let myDocument = document.documentElement;
+let btn = document.getElementById("iframeClose");
+
+btn.addEventListener("click", ()=>{
+    if(btn.textContent == "Go Fullscreen"){
+        if(myDocument.requestFullscreen){
+            myDocument.requestFullscreen();
+        }
+        else if(myDocument.msRequestFullscreen){
+            myDocument.msRequestFullscreen();
+        }
+        else if(myDocument.mozRequestFullscreen){
+            myDocument.mozRequestFullscreen();
+        }
+        else if(myDocument.webkitRequestFullscreen){
+            myDocument.webkitRequestFullscreen();
+        }
+
+        btn.textContent = "Exit Fullscreen";
+    }
+    else{
+        if(document.exitFullscreen){
+            document.exitFullscreen();
+        }
+        else if(document.msexitFullscreen){
+            document.msexitFullscreen();
+        }
+        else if(document.mozexitFullscreen){
+            document.mozexitFullscreen();
+        }
+        else if(document.webkitexitFullscreen){
+            document.webkitexitFullscreen();
+        }
+
+        btn.textContent = "Go Fullscreen";
+    }
+});
