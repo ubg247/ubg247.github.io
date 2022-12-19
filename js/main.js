c6a699233566d0fef23015a9c41396fa04605eab
khanhcx5 <khanhcx5@gmail.com>
2022-12-19 23:05:36 +0700

    update

--- js/main.js
+++ js/main.js
@@ -11,5 +11,48 @@ if (gaenabled == "false") {
       gtag('js', new Date());
       gtag('config', 'G-TREXYT1HKX');`;
   document.head.append(gascript, inlinegascript);
-  script("Injected script 1/5");
+}
+
+
+function addCss(){
+  const e = document.createElement('style');
+  e.innerHTML = ` .iframeClose {
+    position: fixed;
+    left: 5px;
+    top: 5px;
+    background: url(/img/icon-previous.png) no-repeat 50%;
+    background-size: cover;
+    width: 51px;
+    height: 51px;
+    display: none;
+    z-index: 10;
+    cursor: pointer;
+  }
+  .iframeClose.active {
+    display: block;
+  }`;  
+  document.getElementsByTagName('head')[0].appendChild(e);
+}
+
+function addBtnHome(){
+  var e = document.createElement('div');
+  e.className = 'iframeClose active';
+  e.id = 'iframeClose';
+  e.innerHTML = `
+  <div id="iframeClose" class="iframeClose active"></div>`;
+  document.getElementsByTagName('body')[0].appendChild(e);
+}
+
+window.addEventListener('load', function() {
+  addCss();
+  addBtnHome();
+  var btn = document.getElementById("iframeClose");
+  btn.addEventListener("click", returnHome);    
+});
+
+function returnHome(){
+  if(window.hold == true){
+    return;
+  }
+  location.href = "https://ubg247.github.io";
 }
