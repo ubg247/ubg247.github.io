277edc5510499f9ac59f6863d86577f176dae05d
khanhcx5 <khanhcx5@gmail.com>
2022-12-20 00:06:58 +0700

    update

---
+++ js/gtag.js
@@ -0,0 +1,14 @@
+var gaenabled = window.localStorage.getItem("ga");
+if (gaenabled == "false") {
+  script("Skipped GA injection because it is disabled by the user.");
+} else {
+  const gascript = document.createElement("script");
+  gascript.setAttribute("async", "");
+  gascript.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=G-TREXYT1HKX");
+  const inlinegascript = document.createElement("script");
+  inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-TREXYT1HKX');`;
+  document.head.append(gascript, inlinegascript);
+}
